26a6f2461ea6bd6ddcaa2fcb36636fba8700d23b
jkjh8 <jkjh82@naver.com>
2023-07-27 17:29:39 +0900

    add multicast sender

--- src-electron/web/io/commands/index.js
+++ src-electron/web/io/commands/index.js
@@ -8,6 +8,8 @@ const fnPCommands = (args) => {
       openFile(args.values.fullpath)
       logger.info('Open file from ui: ' + args.values.fullpath)
       break
+    case 'play':
+      break
     default:
       console.log('pCommmands: ', JSON.stringify(args))
       break
