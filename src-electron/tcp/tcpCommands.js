9d565a93389f6d9d7c6bbdd1288a7c38d9b4768e
jkjh8 <jkjh82@naver.com>
2023-11-20 21:01:46 +0900

    tcp commands

--- src-electron/tcp/tcpCommands.js
+++ src-electron/tcp/tcpCommands.js
@@ -8,10 +8,11 @@ import { pStatus } from '/src-electron/defaultVal'
 
 export default async function tcpParser(command) {
   try {
-    switch (command) {
-      case 'getfiles':
+    const arr = command.split(',')
+    switch (arr[0]) {
+      case 'getfile':
         return JSON.stringify(getMediaFolder())
-      case 'getfilenames':
+      case 'getfilename':
         return JSON.stringify(getFileNames())
       case 'setloadandplay':
         await dbUpdate('loadandplay', true)
@@ -34,6 +35,28 @@ export default async function tcpParser(command) {
         await dbUpdate('controlport', 4040)
         await updateSetupFromDb()
         return 'ok'
+      case 'getfilename16':
+        break
+      case 'getaudiofilename16':
+        break
+      case 'getvideofilename16':
+        break
+      case 'getimagefilename16':
+        break
+      case 'play':
+        break
+      case 'stop':
+        break
+      case 'pause':
+        break
+      case 'getvolume':
+        break
+      case 'setvolume':
+        break
+      case 'load':
+        // arr[1] filename
+        break
+
       default:
         logger.warn(`Unknown command ${command}`)
         return `Unknown command`
