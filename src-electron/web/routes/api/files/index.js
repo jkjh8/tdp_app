c2bb14d5e5f4fa4bb3e6f7dbcebd8c24590fed22
jkjh8 <jkjh82@naver.com>
2023-12-15 08:46:23 +0900

    update webui

--- src-electron/web/routes/api/files/index.js
+++ src-electron/web/routes/api/files/index.js
@@ -4,7 +4,6 @@ import path from 'path'
 import fs from 'fs'
 import multer from 'multer'
 import logger from '/src-electron/logger'
-
 import { mediaFolder, getMediaFolder } from '/src-electron/fn/folders'
 
 const router = express.Router()
@@ -20,7 +19,7 @@ const upload = multer({
       cb(null, currentPath)
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname)
+      cb(null, decodeURIComponent(file.fieldname))
     }
   })
 })
@@ -44,6 +43,19 @@ router.post('/upload', upload.any(), (req, res) => {
     res.status(200).json({ result: true, files: uploadedFiles })
   } catch (err) {
     logger.error('Web file upload error: ' + err)
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/', async (req, res) => {
+  try {
+    const { fullpath } = req.query
+    if (fs.existsSync(fullpath)) {
+      fs.rmSync(fullpath, { recursive: true })
+    }
+    res.status(200).json({ result: true })
+  } catch (er) {
+    logger.error('Web file delete error: ' + err)
     res.status(500).json(err)
   }
 })
